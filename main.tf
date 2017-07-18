fa6258cd08d99b2845eb47a6494c7644cf5d162d
davetropeano <davetropeano@us.ibm.com>
2017-07-18 09:19:48 -0400

    0.4 provider changes

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 provider "ibm" {
-      bluemix_api_key    = "${var.ibmcloud_bx_api_key}"
+      bluemix_api_key    = "${var.ibm_bx_api_key}"
       #skip_service_configuration = ["softlayer","cluster"]
 }
 
-data "ibmcloud_cf_space" "spacedata" {
+data "ibm_space" "spacedata" {
   space = "${var.space}"
   org   = "${var.org}"
 }
 
-resource "ibmcloud_cf_service_instance" "service-instance" {
+resource "ibm_service_instance" "service-instance" {
   name       = "${var.service_instance_name}"
-  space_guid = "${data.ibmcloud_cf_space.spacedata.id}"
+  space_guid = "${data.ibm_space.spacedata.id}"
   service    = "${var.service}"
   plan       = "${var.plan}"
   tags       = ["cluster-service", "cluster-bind"]
 }
 
-resource "ibmcloud_cf_service_key" "serviceKey" {
+resource "ibm_service_key" "serviceKey" {
   name                  = "${var.service_key_name}"
-  service_instance_guid = "${ibmcloud_cf_service_instance.service-instance.id}"
+  service_instance_guid = "${ibm_service_instance.service-instance.id}"
 }
 
 # Required for the IBM Cloud provider
-variable "ibmcloud_bx_api_key" {}
+variable "ibm_bx_api_key" {}
 
 variable "space" {
   default = "dev"
@@ -50,9 +50,9 @@ variable "service_key_name" {
 
 #service instance guid
 output "guid" {
-  value = "${ibmcloud_cf_service_instance.service-instance.id}"
+  value = "${ibm_service_instance.service-instance.id}"
 }
 
 output "credentials" {
-      value = "${ibmcloud_cf_service_key.serviceKey.credentials}"
+      value = "${ibm_service_key.serviceKey.credentials}"
 }
